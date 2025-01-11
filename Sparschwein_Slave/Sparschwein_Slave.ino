0882cef9748590ec9a712bbff7669d95426bd76d
Denis :) <194630544+DAlt-09@users.noreply.github.com>
2025-01-12 00:42:36 +0100

    Update Sparschwein_Slave.ino
    Strings zu floats

--- Sparschwein_Slave/Sparschwein_Slave.ino
+++ Sparschwein_Slave/Sparschwein_Slave.ino
@@ -10,14 +10,14 @@ void loop() {
   for (int i = 1; i <= 8; i++) {
     if (digitalRead(i)) {
       switch (i) {
-        case 1: Serial.println("0.01"); break;
-        case 2: Serial.println("0.02"); break;
-        case 3: Serial.println("0.05"); break;
-        case 4: Serial.println("0.10"); break;
-        case 5: Serial.println("0.20"); break;
-        case 6: Serial.println("0.50"); break;
-        case 7: Serial.println("1.00"); break;
-        case 8: Serial.println("2.00"); break;
+        case 1: Serial.println(0.01); break;
+        case 2: Serial.println(0.02); break;
+        case 3: Serial.println(0.05); break;
+        case 4: Serial.println(0.10); break;
+        case 5: Serial.println(0.20); break;
+        case 6: Serial.println(0.50); break;
+        case 7: Serial.println(1.00); break;
+        case 8: Serial.println(2.00); break;
       }
     }
   }
